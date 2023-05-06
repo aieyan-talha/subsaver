af40e2d2d24868d7c7703d48fc10a9a0d11a4fba
aieyan-talha <52277557+aieyan-talha@users.noreply.github.com>
2023-05-06 16:48:56 +1000

    Initial commit of the project

---
+++ SubSaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubSaver
+//
+//  Created by Aieyan Talha on 6/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Sub Saver Creators!")
+            Text("Lets Begin!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SubSaver/SubSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubSaverApp.swift
+//  SubSaver
+//
+//  Created by Aieyan Talha on 6/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct SubSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
